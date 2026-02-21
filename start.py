84cb78b226ee4d4b1c649f2db0c4be1d509b7bb5
officialmolotoff <officialmolotoff@gmail.com>
2026-02-21 19:19:08 +0400

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,9 @@
 #стартовая страница
 
 import streamlit as st  
-import pandas as pd  
+import pandas as pd
+
+@st.cache_data #функция кэширует данные из Excel (файл не будет перечитываться при каждом нажатии кнопки, что делает поиск мгновенным)
 
 st.title("Квартиры")
 
@@ -24,3 +26,19 @@ total_area = st.number_input("Общая площадь (в кв. м.):",
 
 # и т. д. (площадь кухни, тип комнат, ...) 
 
+# данные из Excel
+df = pd.read_excel(houses.xlsx)
+# полный адрес для удобства поиска
+df['FullAddress'] = df['Тип улицы'].astype(str) + " " + df['Адрес'].astype(str)
+address_list = df['FullAddress'].tolist()
+# Поисковое поле с предложениями
+# index=None делает поле изначально пустым
+selected_address = st.selectbox(
+            "Начните вводить адрес:",
+            options = address_list,
+            index = None,
+            placeholder = "Например: ул. Омская, д. 21",
+        )
+# Вывод результата
+if selected_address:
+  st.success(f"Вы выбрали: **{selected_address}**")
