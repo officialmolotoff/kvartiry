1d89c7f2e333ba8e5c04955005eb5b23065696f6
officialmolotoff <officialmolotoff@gmail.com>
2026-02-21 20:56:40 +0400

    Update start.py

--- start.py
+++ start.py
@@ -9,14 +9,17 @@ st.write("Данный сайт представляет собой прогно
 
 number_of_rooms = st.number_input("Количество комнат:",
                     min_value = 1,
+                    index = None,
                     step = 1)
 
 floor = st.number_input("Этаж:",
                     min_value = 1,
+                    index = None,
                     step = 1)
 
 total_area = st.number_input("Общая площадь (в кв. м.):",
                               min_value = 0.01,
+                              index = None,
                               step = 0.01)
 
 # и т. д. (площадь кухни, тип комнат, ...) 
