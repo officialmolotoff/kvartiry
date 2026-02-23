cf06cdfdd6755336fd6f1dad5a9e0d6ef0682d5f
officialmolotoff <officialmolotoff@gmail.com>
2026-02-23 22:01:50 +0400

    Update start.py

--- start.py
+++ start.py
@@ -47,7 +47,7 @@ with st.expander("Информация о доме", expanded = True): # expande
            # Получаем строку с данными для выбранного адреса
            # .iloc[0] берет первую (и единственную) найденную строку
            building_details = df[df['FullAddress'] == selected_address].iloc[0]
-           building_details['FullAddress'] = building_details['Тип улицы'].astype(str) + " " + building_details['Адрес'].astype(str)
+           building_details['FullAddress'] = building_details['Тип улицы'] + " " + building_details['Адрес']
 
            #Генерирует HTML/JS компонент с Яндекс.Картой
            html_code = f"""
