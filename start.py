823c14a423d14174f5d8a524021fef40880168be
officialmolotoff <officialmolotoff@gmail.com>
2026-02-23 19:31:07 +0400

    Update start.py

--- start.py
+++ start.py
@@ -3,19 +3,22 @@
 import streamlit as st  
 import pandas as pd
 
+st.set_page_config(page_title = "Прогнозирование стоимости квартиры", layout = "centered")
+
 st.title("Квартиры")
 
 st.write("Данный сайт представляет собой прогноз стоимости квартиры по введённым параметрам.")
 
-number_of_rooms = st.number_input("Количество комнат:",
+with st.expander("Данные о квартире", expanded = True): # expanded=True означает, что по умолчанию вкладка открыта
+  number_of_rooms = st.number_input("Количество комнат:",
                     min_value = 1,
                     step = 1)
 
-floor = st.number_input("Этаж:",
+  floor = st.number_input("Этаж:",
                     min_value = 1,
                     step = 1)
 
-total_area = st.number_input("Общая площадь (в кв. м.):",
+  total_area = st.number_input("Общая площадь (в кв. м.):",
                               min_value = 0.01,
                               step = 0.01)
 
@@ -27,14 +30,15 @@ df = pd.read_excel("houses.xlsx")
 # полный адрес (тип улицы + адрес) для удобства поиска
 df['FullAddress'] = df['Тип улицы'].astype(str) + " " + df['Адрес'].astype(str)
 address_list = df['FullAddress'].tolist()
-# Поисковое поле с предложениями
-# index=None делает поле изначально пустым
-selected_address = st.selectbox(
+with st.expander("Данные о доме", expanded = True): # expanded=False означает, что по умолчанию вкладка закрыта
+  # Поисковое поле с предложениями
+  # index=None делает поле изначально пустым
+  selected_address = st.selectbox(
             "Начните вводить адрес:",
             options = address_list,
             index = None,
             placeholder = "Например: улица омская, 21",
-        )
+          )
 # Вывод результата
 #if selected_address:
 #  st.success(f"Вы выбрали: **{selected_address}**")
